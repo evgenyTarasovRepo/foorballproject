ca7d12decc1ab55a871174359a8c1988b737202d
Evgeny Tarasov <eatarasov@yahoo.com>
2024-03-22 16:46:51 +0300

    initial commit

---
+++ src/main/java/com/tarasov/footballproject/FootballprojectApplication.java
@@ -0,0 +1,13 @@
+package com.tarasov.footballproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarasov/footballproject/FootballprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarasov.footballproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
